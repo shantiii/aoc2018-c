71b388f6ba78b84642afaa44f7f13994d88bbdf4
Shanti Chellaram <shanti@shanti.im>
2019-01-04 22:51:03 -0500

    Clean up main

--- main.c
+++ main.c
@@ -4,21 +4,50 @@
 #include <err.h>
 #include <dlfcn.h>
 
-#include "vector.h"
 #include "problem.h"
 
 #define NUM_PROBLEMS 25
 
-struct box_set_node {
-	unsigned int y_pos, y_len;
-	struct interval_set *row_data;
-};
-
-struct box_set {
-	struct box_set_node *root;
-};
-
 typedef void (*problem_fn)(FILE *, struct solution *);
+
+/* output the "how to use explanation" */
+void print_usage();
+/* dynamically load solvers into provided array */
+void scan_solvers(problem_fn *, size_t);
+
+/// ENTRY POINT
+int main(int argc, char *argv[]) {
+	/* the problem input file */
+	FILE *input_file;
+	/* the selected problem */
+	size_t problem_id;
+	/* storage for the solution */
+	struct solution my_solution = { .answered = 0 };
+	/* array of pointers to solver functions */
+	problem_fn solvers[NUM_PROBLEMS] = {0};
+
+	/* dynamically load solvers */
+	scan_solvers(solvers, NUM_PROBLEMS);
+
+	/* input validation */
+	if (argc != 3) {
+		print_usage();
+		exit(EX_USAGE);
+	}
+	if (sscanf(argv[1], "%zd", &problem_id) != 1 || problem_id < 1 || problem_id > NUM_PROBLEMS) {
+		errx(EX_USAGE, "invalid problem: %s", argv[1]);
+	}
+	if (solvers[problem_id-1] == NULL) {
+		errx(EX_USAGE, "problem %zd not yet solved", problem_id);
+	}
+	if ((input_file = fopen(argv[2], "r")) == NULL) {
+		errx(EX_NOINPUT, "unable to open file '%s'", argv[2]);
+	}
+
+	solvers[problem_id-1](input_file, &my_solution);
+	print_solution(&my_solution);
+	return 0;
+}
 
 void print_usage() {
 	fprintf(stderr, "usage: aoc <problem #> <input file>\n");
@@ -38,37 +67,3 @@ void scan_symbols(problem_fn *solvers, size_t num_solvers) {
 		}
 	}
 }
-
-/// ENTRY POINT
-int main(int argc, char *argv[]) {
-	/* the problem input file */
-	FILE *input_file;
-	/* the selected problem */
-	size_t problem_id;
-	/* storage for the solution */
-	struct solution my_solution = { .answered = 0 };
-	/* array of pointers to solver functions */
-	problem_fn solvers[NUM_PROBLEMS] = {0};
-
-	/* dynamically load solvers */
-	scan_symbols(solvers, NUM_PROBLEMS);
-
-	if (argc != 3) {
-		print_usage();
-		exit(EX_USAGE);
-	}
-	if (sscanf(argv[1], "%zd", &problem_id) != 1 || problem_id < 1 || problem_id > NUM_PROBLEMS) {
-		errx(EX_USAGE, "invalid id: %s", argv[1]);
-	}
-	if (solvers[problem_id-1] == NULL) {
-		errx(EX_USAGE, "problem %zd not yet solved", problem_id);
-	}
-	if ((input_file = fopen(argv[2], "r")) == NULL) {
-		errx(EX_NOINPUT, "unable to open file '%s'", argv[2]);
-	}
-
-	solvers[problem_id-1](input_file, &my_solution);
-	print_solution(&my_solution);
-	return 0;
-}
-
